45c10736cb5ebdeac83d0ac0b51d4d26197fb23e
tanaka9230 <tanaka9230@users.noreply.github.com>
2015-12-11 22:29:05 +0900

    added codes for Maven proj. updating test on Eclipse

--- source/main/java/example/Sub.java
+++ source/main/java/example/Sub.java
@@ -1,8 +1,11 @@
 package example;
 
+//import static com.google.common.base.Preconditions.checkNotNull;
+
 public class Sub
 {
     public static String Text(String a) {
         return String.format("Hello %s!", a);
+        //return String.format("Gooooood Morning %s!", checkNotNull(a));
     }
 }
